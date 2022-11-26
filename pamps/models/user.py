f91fc4bb0403f38a57745ce1c5809cfca43fc1be
Gabriel Mizuno <gabrielmizuno@gmail.com>
2022-11-26 13:53:27 -0300

    Create Token

--- pamps/models/user.py
+++ pamps/models/user.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
 from pamps.security import HashedPassword
+from pydantic import BaseModel
 
 
 class User(SQLModel, table=True):
@@ -11,3 +12,19 @@ class User(SQLModel, table=True):
     avatar: Optional[str] = None
     bio: Optional[str] = None
     password: HashedPassword
+
+
+class UserResponse(BaseModel):
+    """Serializer for User Response"""
+    username: str
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+
+
+class UserRequest(BaseModel):
+    """Serializer for User request payload"""
+    email: str
+    username: str
+    password: str
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
